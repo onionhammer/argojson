2afe91816f990ba0c1d73c645e9cdba59aafe175
Erik O'Leary <erik.m.oleary@gmail.com>
2015-03-16 22:33:43 -0500

    Started unit testing

--- ArgoJson.Test/TestArraySerialization.cs
+++ ArgoJson.Test/TestArraySerialization.cs
@@ -11,6 +11,23 @@ namespace ArgoJson.Test
         {
             var array  = new[] { 0, 1, 2, 3, 4 };
             var result = ArgoJson.Serializer.Serialize(array);
+
+            Assert.AreEqual("[0,1,2,3,4]", result);
+        }
+
+        [TestMethod]
+        public void TestObject()
+        {
+            var obj = new
+            {
+                Name = "John Smith",
+                Address = "1912 Franklin Ave\nApt. 221",
+                Age = 22
+            };
+
+            var result = ArgoJson.Serializer.Serialize(obj);
+
+            //Assert.AreEqual("{\"Name\":\"John Smith\",\"Address\":\"1912 Franklin Ave\\nApt. 221\",}");
         }
     }
 }
