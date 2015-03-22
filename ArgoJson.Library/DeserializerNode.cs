8bcb5bb0fc71575493ec9021c52a4cb5e43c34f7
Erik O'Leary <erik.m.oleary@gmail.com>
2015-03-22 12:27:45 -0500

    Started deserializer node

---
+++ ArgoJson.Library/DeserializerNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgoJson
+{
+    internal struct DeserializerNode
+    {
+        #region Fields
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Constructor
+
+        public DeserializerNode(Type type)
+        {
+
+        }
+
+        #endregion
+    }
+}
